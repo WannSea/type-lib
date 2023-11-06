04f3f79297f4425735d512647e7214e505777d06
Kolja Kieslich <kolja.kieslich@gmail.com>
2023-11-06 01:20:51 +0100

    wuff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,23 +1,18 @@
 pub mod config;
-use std::{time::{SystemTime, UNIX_EPOCH}, fmt};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use config::generated::MetricId;
 
 #[derive(Clone, Debug)]
 pub struct MetricByteValue(Vec<u8>);
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MetricMessage {
     ts: u128,
     id: MetricId,
     data: MetricByteValue
 }
 
-impl fmt::Debug for MetricMessage {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("MetricMessage").field("ts", &self.ts).field("id", &self.id).field("data", &self.data).finish()
-    }
-}
 
 impl Into<MetricByteValue> for f32 {
     fn into(self) -> MetricByteValue {
