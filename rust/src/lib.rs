fdd74f510928573945c781cbe648f1d104c233d3
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-07 12:28:05 +0100

    update namespace

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,8 @@
-pub mod wannsea {
-    pub mod proto {
-        include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
-    }
-
-    pub mod types {
-        include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
-    }
+pub mod proto {
+    include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
 }
+
+pub mod types {
+    include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+}
+
