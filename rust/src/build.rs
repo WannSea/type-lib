2e72599a9cc8e718055884f8b45837b479b7648c
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-07 15:41:17 +0100

    add serde

--- rust/src/build.rs
+++ rust/src/build.rs
@@ -1,7 +1,31 @@
+use std::{path::PathBuf, env};
+
 // ToDo: move to build script
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.compile_protos(&["../protos/wannsea.proto"], &["../protos/"])?;
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../").join("protos");
+    let proto_files = vec![root.join("wannsea.proto")];
+    
+    // Tell cargo to recompile if any of these proto files are changed
+    for proto_file in &proto_files {
+        println!("cargo:rerun-if-changed={}", proto_file.display());
+    }
+    
+    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap())
+        .join("proto_descriptor.bin");
+    
+    prost_build::Config::new()
+        // Save descriptors to file
+        .file_descriptor_set_path(&descriptor_path)
+        // Override prost-types with pbjson-types
+        .compile_well_known_types()
+        .extern_path(".google.protobuf", "::pbjson_types")
+        // Generate prost structs
+        .compile_protos(&proto_files, &[root])?;
+    
+    let descriptor_set = std::fs::read(descriptor_path)?;
+    pbjson_build::Builder::new()
+        .register_descriptors(&descriptor_set)?
+        .build(&[".wannsea"])?;
 
     Ok(())
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,2 @@
 include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
+include!(concat!(env!("OUT_DIR"), "/wannsea.proto.serde.rs"));
