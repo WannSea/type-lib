b67e03933531d611783b15fd8aefdfbfda50df12
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-07 12:18:57 +0100

    Use protobuf instead of own mp

--- rust/src/build.rs
+++ rust/src/build.rs
@@ -1,8 +1,7 @@
 use std::{path::Path, env};
-use codegen::{Scope, Impl, Enum};
+use codegen::{Scope, Enum};
 
 static METRIC_ID_ENUM: &str = "MetricId";
-static METRIC_TYPES: &'static [&str] = &["String", "u8", "u16", "i16", "f32", "u64"];
 
 #[derive(Debug, serde::Deserialize, PartialEq)]
 pub struct MetricDefinition {
@@ -14,20 +13,6 @@ pub struct MetricDefinition {
 #[derive(Debug, serde::Deserialize, PartialEq)]
 pub struct Config {
     pub metrics: Vec<MetricDefinition>
-}
-
-fn gen_metric_type_enum() -> Enum {
-    let mut new_enum = Enum::new("MetricType");
-    let metric_type_enum = new_enum
-    .allow("non_camel_case_types")
-    .vis("pub")
-    .derive("Clone")
-    .derive("Debug")
-    .derive("Copy");
-    for metric_type in METRIC_TYPES {
-        metric_type_enum.new_variant(metric_type.to_string());
-    }
-    return new_enum;
 }
 
 fn gen_metric_ids_enum(config: &Config) -> Enum {
@@ -48,23 +33,10 @@ fn gen_metric_ids_enum(config: &Config) -> Enum {
     return new_enum;
 }
 
-fn gen_metric_id_impl(config: &Config) -> Impl {
-    let mut new_impl = Impl::new(METRIC_ID_ENUM);
-    let metrics_impl_fn = new_impl
-    .new_fn("get_type")
-    .arg_ref_self()
-    .ret("MetricType")
-    .vis("pub")
-    .line("match self {");
-    for metric in &config.metrics {
-        metrics_impl_fn.line(format!("{}::{} => MetricType::{},", METRIC_ID_ENUM, metric.name, metric.metric_type));
-    }
-    metrics_impl_fn.line("}");
-    return new_impl;
-}
-
 // ToDo: move to build script
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    prost_build::compile_protos(&["src/wannsea.proto"], &["src/"])?;
+
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("codegen.rs");
 
     let f = std::fs::File::open("../metrics.yaml")?;
@@ -75,9 +47,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     scope.import("strum_macros", "Display");
     scope.import("strum_macros", "FromRepr");
 
-    scope.push_enum(gen_metric_type_enum());
     scope.push_enum(gen_metric_ids_enum(&d));
-    scope.push_impl(gen_metric_id_impl(&d));
    
     std::fs::write(path, scope.to_string())?;
 
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,49 +1,5 @@
-use std::{time::{SystemTime, UNIX_EPOCH}, error::Error};
-use types::{MetricByteValue, ByteParseError};
-pub mod types;
-
-include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
-
-#[derive(Clone, Debug)]
-pub struct MetricMessage {
-    pub ts: u128,
-    pub id: MetricId,
-    pub data: MetricByteValue
-}
-
-impl MetricMessage {
-    pub fn now(id: MetricId, data: MetricByteValue) -> Self {
-        MetricMessage { id, data, ts: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() }
-    }
-
-    pub fn get_json_data(&self) -> Result<String, ByteParseError> {
-        match self.id.get_type() {
-            MetricType::u8 => Ok(u8::try_from(self.data.clone())?.to_string()),
-            MetricType::i16 => Ok(i16::try_from(self.data.clone())?.to_string()),
-            MetricType::u16 => Ok(u16::try_from(self.data.clone())?.to_string()),
-            MetricType::f32 => Ok(f32::try_from(self.data.clone())?.to_string()),
-            MetricType::u64 => Ok(u64::try_from(self.data.clone())?.to_string()),
-            MetricType::String => Ok(format!("\"{}\"", String::try_from(self.data.clone())?.to_string())),
-        }
-    }
-
-    pub fn get_json_repr(&self) -> Result<String, ByteParseError> {
-        Ok(format!("{{ \"ts\": {}, \"id\": \"{}\", \"data\": {} }}", self.ts, self.id.to_string(), self.get_json_data()?))
-    }
-}
-
-impl From<MetricMessage> for Vec<u8> {
-    fn from(value: MetricMessage) -> Self {
-        let mut out = Vec::new();
-        out.extend_from_slice(&value.ts.to_be_bytes());
-        out.push(value.id as u8);
-        out.extend(Vec::from(value.data));
-        return out;
-    }
-}
-
-impl From<Vec<u8>> for MetricMessage {
-    fn from(value: Vec<u8>) -> Self {
-        MetricMessage { ts: u128::from_be_bytes(value[0..16].try_into().unwrap()), id: MetricId::from_repr(value[16] as usize).unwrap(), data: MetricByteValue(value[17..].to_vec()) }
+pub mod wannsea {
+    pub mod proto {
+        include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
     }
 }
