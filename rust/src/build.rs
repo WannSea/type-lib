e570327a6c6121d3542efcf638e9e213f916c1da
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-07 15:22:23 +0100

    transformation

--- rust/src/build.rs
+++ rust/src/build.rs
@@ -1,7 +1,7 @@
 // ToDo: move to build script
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
-    config.compile_protos(&["src/wannsea.proto"], &["src/"])?;
+    config.compile_protos(&["../protos/wannsea.proto"], &["../protos/"])?;
 
     Ok(())
 }
