ce24081de50b550139ed926ac45bce4845e26e6d
Kolja Kieslich <kolja.kieslich@gmail.com>
2024-01-10 12:07:46 +0100

    Test: Remove pbjson

--- rust/src/build.rs
+++ rust/src/build.rs
@@ -10,22 +10,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("cargo:rerun-if-changed={}", proto_file.display());
     }
     
-    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap())
-        .join("proto_descriptor.bin");
-    
     prost_build::Config::new()
-        // Save descriptors to file
-        .file_descriptor_set_path(&descriptor_path)
-        // Override prost-types with pbjson-types
-        .compile_well_known_types()
-        .extern_path(".google.protobuf", "::pbjson_types")
-        // Generate prost structs
+        .type_attribute(".", "#[derive(serde::Serialize)]")
         .compile_protos(&proto_files, &[root])?;
     
-    let descriptor_set = std::fs::read(descriptor_path)?;
-    pbjson_build::Builder::new()
-        .register_descriptors(&descriptor_set)?
-        .build(&[".wannsea"])?;
-
     Ok(())
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,2 +1,4 @@
+extern crate serde;
+
 include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
-include!(concat!(env!("OUT_DIR"), "/wannsea.proto.serde.rs"));
+// include!(concat!(env!("OUT_DIR"), "/wannsea.proto.serde.rs"));
