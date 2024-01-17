7f4b339b8a0f80786536a32ef097d817cfb71280
Kolja <kolja.kieslich@gmail.com>
2024-01-17 11:25:52 +0100

    impl partialeq for i32

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,7 @@
 include!(concat!(env!("OUT_DIR"), "/wannsea.proto.rs"));
+
+impl PartialEq<i32> for MessageId {
+    fn eq(&self, other: &i32) -> bool {
+        return *other == (*self as i32);
+    }
+}
